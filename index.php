96184fc4e189f48b8ee6e37050a31ebb244d6c66
Student User <student@workstation.lab.example.com>
2022-10-17 06:32:27 -0400

    index was changed

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<html> 
-	<head> 
-	<title>PHP Test</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
-	</head>
+<?php
+print "print does not require parentheses.";
 
-<body> 
-	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
-		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>GOING ON VACATION</h2>
-	</body> 
-</html>
+// No newline or space is added; the below outputs "helloworld" all on one line
+// print "hello";
+// print "world";
+//
+// print "This string spans
+// multiple lines. The newlines will be
+// output as well";
+//
+// print "This string spans\nmultiple lines. The newlines will be\noutput as well.";
+//
+// // The argument can be any expression which produces a string
+// $foo = "example";
+// print "foo is $foo"; // foo is example
+//
+// $fruits = ["lemon", "orange", "banana"];
+// print implode(" and ", $fruits); // lemon and orange and banana
+//
+// // Non-string expressions are coerced to string, even if declare(strict_types=1) is used
+// print 6 * 7; // 42
+//
+// // Because print has a return value, it can be used in expressions
+// // The following outputs "hello world"
+// if ( print "hello" ) {
+//     echo " world";
+//     }
+//
+//     // The following outputs "true"
+//     ( 1 === 1 ) ? print 'true' : print 'false';
+//     ?>
